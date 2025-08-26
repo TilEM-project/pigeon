588f0e9f4ada447b01941caab9f672005d45632e
Cameron Devine <cameron.devine@alleninstitute.org>
2025-08-26 07:25:26 -0700

    new options, bug fixes, and other improvements for the cli

--- pigeon/__main__.py
+++ pigeon/__main__.py
@@ -3,14 +3,20 @@ import argparse
 import yaml
 from functools import partial
 import json
+from inspect import getsource
+from textwrap import indent
+from os import environ
 
 
 class Listener:
-    def __init__(self, disp_headers, record):
+    def __init__(self, disp_headers, record=False, rate=False, quiet=False):
         self.message_received = False
         self.disp_headers = disp_headers
         self.record = record
+        self.rate = rate
+        self.quiet = quiet
         self.messages = []
+        self.last_timestamp = None
 
     def callback(self, msg, topic, headers):
         if self.record:
@@ -21,13 +27,24 @@ class Listener:
                     "headers": headers,
                 }
             )
-        else:
+        if not self.quiet:
             print(f"Recieved message on topic '{topic}':")
             print(msg)
             if self.disp_headers:
                 print("With headers:")
                 for key, val in headers.items():
                     print(f"{key}={val}")
+        if self.rate:
+            if self.last_timestamp is not None:
+                try:
+                    rate = 1000 / (
+                        int(headers.get("sent_at")) - int(self.last_timestamp)
+                    )
+                    print(f"Rate: {rate} Hz")
+                except ValueError as e:
+                    print("Could not parse timestamp:")
+                    print(e)
+            self.last_timestamp = headers.get("sent_at")
         self.message_received = True
 
     def write(self, path):
@@ -40,21 +57,22 @@ def main():
     parser.add_argument(
         "--host",
         type=str,
-        default="127.0.0.1",
-        help="The message broker to connect to.",
+        help="The message broker to connect to. Defaults to the PIGEON_HOST environment variable if set, otherwise 127.0.0.1.",
     )
     parser.add_argument(
-        "--port", type=int, default=61616, help="The port to use for the connection."
+        "--port",
+        type=int,
+        help="The port to use for the connection. Defaults to the PIGEON_PORT environment variable if set, otherwise 61616.",
     )
     parser.add_argument(
         "--username",
         type=str,
-        help="The username to use when connecting to the STOMP server.",
+        help="The username to use when connecting to the STOMP server. The environment variable PIGEON_USERNAME is used if set.",
     )
     parser.add_argument(
         "--password",
         type=str,
-        help="The password to use when connecting to the STOMP server.",
+        help="The password to use when connecting to the STOMP server. The environment variable PIGEON_PASSWORD is used if set.",
     )
     parser.add_argument(
         "-p", "--publish", type=str, help="The topic to publish a message to."
@@ -77,47 +95,81 @@ def main():
         "-r", "--record", type=str, help="Write received messages to a JSON file."
     )
     parser.add_argument(
-        "--one", action="store_true", help="Exit after receiving one message."
+        "--rate",
+        action="store_true",
+        help="Display the rate at which messages are received.",
+    )
+    parser.add_argument(
+        "-1", "--one", action="store_true", help="Exit after receiving one message."
     )
     parser.add_argument(
         "-l", "--list", action="store_true", help="List registered topics and exit."
     )
     parser.add_argument(
+        "--show",
+        action="append",
+        default=[],
+        type=str,
+        help="Show the message definition of the specified topic.",
+    )
+    parser.add_argument(
         "--headers", action="store_true", help="Display headers of received messages."
+    )
+    parser.add_argument(
+        "-q", "--quiet", action="store_true", help="Don't display incoming messages."
     )
 
     args = parser.parse_args()
 
+    connection = Pigeon(
+        "CLI",
+        environ.get("PIGEON_HOST", "127.0.0.1") if args.host is None else args.host,
+        environ.get("PIGEON_PORT", 61616) if args.port is None else args.port,
+    )
+
     if args.list:
-        connection = Pigeon("CLI")
         for topic in connection._topics:
             print(topic)
-        return
+
+    for topic in args.show:
+        if topic not in connection._topics:
+            print(f"Topic {topic} not defined!")
+            continue
+        print(f"{topic}:")
+        print(indent(getsource(connection._topics[topic]), "    "))
 
     if args.publish is None and args.subscribe is None and not args.all:
         print("No action specified.")
-        return
+        exit(1)
 
     if args.publish and args.data is None:
         print("Must also specify data to publish.")
-        return
+        exit(1)
 
     if args.data and args.publish is None:
         print("Must also specify topic to publish data to.")
-        return
+        exit(1)
 
     if args.record is not None and not (args.subscribe or args.all):
         print("No subscriptions to record.")
-        return
+        exit(1)
 
-    connection = Pigeon("CLI", args.host, args.port)
-    connection.connect(args.username, args.password)
+    if args.publish or args.subscribe or args.all:
+        connection.connect(
+            environ.get("PIGEON_USERNAME") if args.username is None else args.username,
+            environ.get("PIGEON_PASSWORD") if args.password is None else args.password,
+        )
 
     if args.publish:
         connection.send(args.publish, **yaml.safe_load(args.data))
 
     if args.subscribe or args.all:
-        listener = Listener(args.headers, args.record is not None)
+        listener = Listener(
+            args.headers,
+            record=args.record is not None,
+            rate=args.rate,
+            quiet=args.quiet,
+        )
 
     if args.all:
         connection.subscribe_all(listener.callback)
@@ -130,9 +182,10 @@ def main():
             while not (args.one and listener.message_received):
                 pass
         except KeyboardInterrupt:
+            print("exiting")
+        finally:
             if args.record is not None:
                 listener.write(args.record)
-            print("exiting")
     exit(0)
 
 
